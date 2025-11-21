81c5a63bebf13d6892dbbd6ba5d0bf3dc70bea53
Hirwa Tresor <waveb6133@gmail.com>
2025-11-21 23:24:55 +0200

    added fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,32 +1,12 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config();
-const uri = process.env.MONGO_URI;
-
-if (!uri) {
-  console.error("❌ MONGO_URI is missing in your .env file");
-  process.exit(1);
-}
-const client = new MongoClient(uri, {
-  tls: true,
-  tlsAllowInvalidCertificates: true, // <-- important for Render
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function connectDB() {
-  try {
-    await client.connect();
-
-    console.log("✅ Successfully connected to MongoDB Atlas!");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1);
-  }
+const connectDB = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to the db");        
+    } catch (error) {
+        console.error("error connecting to the DB");        
+    }
 }
 
 export default connectDB;
